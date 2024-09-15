d350a8613b24ef694c7e6d8c976825dcb8c7fd37
mike-alexander-smith <48382529+mike-alexander-smith@users.noreply.github.com>
2024-09-15 11:10:09 -0400

    Update startDocker.sh

--- startDocker.sh
+++ startDocker.sh
@@ -1,3 +1,25 @@
-docker run -v /home/mike_alexander_smith:/home/rstudio \
+sudo apt-get update
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+    
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null  
+  
+  
+sudo apt-get update
+
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+
+
+sudo docker run -v /home/mike_alexander_smith:/home/rstudio \
     -e PASSWORD=hyperion1234 -p 8787:8787  \
     ghcr.io/bodenmillergroup/imcdataanalysis:latest
